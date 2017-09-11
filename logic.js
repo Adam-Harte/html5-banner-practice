7279a98b826f6314f9696922d093aabf05322db4
Adam-Harte <adamharte11@hotmail.co.uk>
2017-09-11 23:00:35 +0100

    basic structure for html5 banner with animations created

---
+++ logic.js
@@ -0,0 +1,78 @@
+
+// Reference to the creative's various properties and elements.
+var creative = {};
+
+
+/**
+ * Called on the window load event.
+ */
+function preInit() {
+  setupDom();
+
+  if (Enabler.isInitialized()) {
+    init();
+  } else {
+    Enabler.addEventListener(
+      studio.events.StudioEvent.INIT,
+      init
+    );
+  }
+}
+
+/**
+ * Set up references to DOM elements.
+ */
+function setupDom() {
+  creative.dom = {};
+  creative.dom.mainContainer = document.getElementById('main-container');
+  creative.dom.exit = document.getElementById('exit');
+  creative.dom.banner = document.getElementById('banner');
+}
+
+/**
+ * The Enabler is now initialized and any extra modules have been loaded.
+ */
+function init() {
+  addListeners();
+  // Polite loading
+  if (Enabler.isVisible()) {
+    show();
+  }
+  else {
+    Enabler.addEventListener(studio.events.StudioEvent.VISIBLE, show);
+  }
+}
+
+/**
+ * Add appropriate listeners after the creative's DOM has been set up.
+ */
+function addListeners() {
+  creative.dom.exit.addEventListener('click', exitClickHandler);
+}
+
+function animate() {
+  setTimeout(function() {
+    creative.dom.banner.style.transform = 'translateY(0px)';
+  }, 1000)
+}
+
+/**
+ *  Shows the ad.
+ */
+function show() {
+  creative.dom.exit.style.display = "block";
+  animate();
+}
+
+// ---------------------------------------------------------------------------------
+// MAIN
+// ---------------------------------------------------------------------------------
+
+function exitClickHandler() {
+  Enabler.exit('BackgroundExit');
+}
+
+/**
+ *  Main onload handler
+ */
+window.addEventListener('load', preInit);
